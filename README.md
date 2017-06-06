a1284376183a40395d344efc2e81fbdecd59985b
WJerry0227 <wangjiawei0227@163.com>
2017-06-06 15:40:33 +0800

    Finish ass and srt

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Subtitle Transfer
-It is a small python program to remove Chinese characters in subtitle
+
+## Intro
+It is a small python program to **remove Chinese or English** characters in your subtitle file
 
 When I was watching *This is us* on my computer. I hate the Chinese subtitle mixed with English subtitle.
 
 Therefore, this python program came to mind.
 
-# usage
+## Usage
 
+```
 python SubtitleTransfer.py [YOUR SUBTITLE FILE PATH] [TYPE]
-
-> TYPE: [Chinese] only now
+```
+> - TYPE: The type that you want to delete from your subtitle.
+> - [Chinese] [English] are available only
+> - *.ass* and *.srt* are available only
 
 It will create a new file in your old path with the new postfix.
+
+## Encoding
+
+The encoding of your subtitle will be automatically detected.
--- SubtitleTransfer.py
+++ SubtitleTransfer.py
@@ -1,7 +1,7 @@
 # *_* coding:utf-8 *_*
 import sys
 import re
-
+import chardet
 
 class SubtitleTransfer:
     """
@@ -15,8 +15,18 @@ class SubtitleTransfer:
 
         """
         self.filename = filename
+        self.file = open(filename)
+
+        self.newFileName = ""
+        code = chardet.detect(self.file.read())
+        self.encoding = code['encoding']
+        print "Encoding Type : " + self.encoding + ", Confidence : " + str(code['confidence'])
+
         spilts = filename.split('.')
-        self.newFileName = spilts[0] + "_new." + filename.split('.')[len(spilts)-1]
+        for i in range(0,len(spilts)-1):
+            self.newFileName += spilts[i]
+            self.newFileName += "."
+        self.newFileName += "_new_no" +type + "." +spilts[len(spilts)-1]
         self.type = type
 
     def is_chinese(self, uchar):
@@ -28,24 +38,95 @@ class SubtitleTransfer:
         else:
             return False
 
+    def is_alphabet(self,uchar):
+        """judge whether unicode is an alphabet"""
+        if (uchar >= u'\u0041' and uchar <= u'\u005a') or (uchar >= u'\u0061' and uchar <= u'\u007a'):
+            return True
+        else:
+            return False
+
     def del_Chinese_Ass(self):
         """
         del ass Chinese subtitle
         """
         file = open(self.filename, 'r')
         newfile = open(self.newFileName, 'w')
-
         for line in file.readlines():
-            # print line.decode('utf-8')
             if line.startswith("Dialogue"):
-                for char in line.decode('utf-8'):
-                    if self.is_chinese(char):
-                        line = line.replace(char.encode('utf-8'), '')
-                newfile.write(line)
+                lineSplits = line.split(",")
+                content = ""
+                prefix = ""
+                i = 0
+                while i < len(lineSplits):
+                    if i < 9:
+                        prefix += lineSplits[i]
+                        prefix += ","
+                    elif i < len(lineSplits) - 1:
+                        content += lineSplits[i]
+                        content += ","
+                    else:
+                        content += lineSplits[i]
+                    i += 1
 
+                matchTags = re.findall("\{.*\}", line)
+
+                for matchTag in matchTags:
+                    content = content.replace(matchTag, "")
+                contentSplits = content.split("\N")
+                for contentSplit in contentSplits:
+                    for char in contentSplit.decode(self.encoding, 'ignore'):
+                        if self.is_chinese(char):
+                            line = line.replace(contentSplit, "")
+                            break
+
+                newfile.write(line)
             else:
                 newfile.write(line)
         print "Delete Chinese Subtitle Successfully."
+
+    def del_English_Ass(self):
+        """
+        del ass English subtitle
+        """
+        file = open(self.filename, 'r')
+        newfile = open(self.newFileName, 'w')
+        for line in file.readlines():
+            if line.startswith("Dialogue"):
+                lineSplits = line.split(",")
+                content = ""
+                prefix = ""
+                i = 0
+                while i < len(lineSplits):
+                    if i < 9:
+                        prefix += lineSplits[i]
+                        prefix += ","
+                    elif i < len(lineSplits)-1:
+                        content += lineSplits[i]
+                        content += ","
+                    else:
+                        content += lineSplits[i]
+                    i += 1
+
+                matchTags = re.findall("\{.*\}", line)
+
+                for matchTag in matchTags:
+                    content = content.replace(matchTag, "")
+                contentSplits = content.split("\N")
+                for contentSplit in contentSplits:
+                    isChineseLine = False
+                    for char in contentSplit.decode(self.encoding, 'ignore'):
+                        if self.is_chinese(char):
+                            isChineseLine = True
+                            break
+                    if not isChineseLine:
+                        line = line.replace(contentSplit,"")
+                if not line.endswith("\n"):
+                    line += "\n"
+                newfile.write(line)
+            else:
+                newfile.write(line)
+        print "Delete English Subtitle Successfully."
+
 
 
     def del_Chinese_Srt(self):
@@ -57,13 +138,43 @@ class SubtitleTransfer:
 
         for line in file.readlines():
             isChineseLine = False
-            for char in line.decode("utf-8"):
+            for char in line.decode(self.encoding,'ignore'):
                 if self.is_chinese(char):
                     isChineseLine = True
                     break
             if not isChineseLine:
                 newFile.write(line)
         print "Delete Chinese Subtitle Successfully."
+
+    def del_English_Srt(self):
+        """
+        del srt English subtitle
+        """
+        file = open(filename, 'r')
+        newFile = open(self.newFileName, 'w')
+
+        for line in file.readlines():
+            isChineseLine = False
+            isEnglishLine = False
+            matchTags = re.findall("\{.*\}", line)
+            oldline = line
+            for matchTag in matchTags:
+                line = line.replace(matchTag, "")
+
+            for char in line.decode(self.encoding, 'ignore'):
+                if self.is_chinese(char):
+                    isChineseLine = True
+                    break
+                if self.is_alphabet(char):
+                    isEnglishLine = True
+            if isChineseLine:
+                newFile.write(oldline)
+                continue
+            if isEnglishLine:
+                continue
+            newFile.write(oldline)
+
+        print "Delete English Subtitle Successfully."
 
 
     def transfer(self):
@@ -73,12 +184,14 @@ class SubtitleTransfer:
         if self.filename.endswith(".ass"):
             if self.type == "Chinese":
                 self.del_Chinese_Ass()
+            elif self.type == "English":
+                self.del_English_Ass()
 
         elif self.filename.endswith(".srt"):
             if self.type == "Chinese":
                 self.del_Chinese_Srt()
-
-
+            elif self.type == "English":
+                self.del_English_Srt()
 
 
 if __name__ == '__main__':
